4260743e90c4d2d58714f095763451a825e6ade1
SwatejPatil <81546709+SwatejPatil@users.noreply.github.com>
2021-08-08 00:50:19 -0700

    Added Open Feature

--- veronica.py
+++ veronica.py
@@ -3,7 +3,9 @@ import pyttsx3
 import datetime
 import speech_recognition as sr
 import pyaudio
-
+import wikipedia
+import webbrowser
+import os
 
 engine = pyttsx3.init('sapi5')
 voices = engine.getProperty('voices')
@@ -19,13 +21,14 @@ def wishMe():
     hour = int(datetime.datetime.now().hour)
     if hour >= 0 and hour < 12:
         speak("Good Morning!")
+
     elif hour >= 12 and hour < 18:
-        speak("Good Afternoon")
+        speak("Good Afternoon!")
 
     else:
         speak("Good Evening")
 
-        speak("I am Veronica, How can I help you")
+    speak("I am Veronica, How can I help you")
 
 
 def takeCommand():  # input Voice
@@ -41,12 +44,36 @@ def takeCommand():  # input Voice
         print(f"User said: {query}\n")
 
     except Exception as e:
-        print(e)
-        
         print("Say that again please...")
         return "None"
     return query
 
+
 if __name__ == "__main__":
     wishMe()
-    takeCommand()
+    while True:
+        query = takeCommand().lower()
+
+        if 'wikipedia' in query:
+            speak('Searching Wikipedia...')
+            query = query.replace("wikipedia", "")
+            results = wikipedia.summary(query, sentences=2)
+            speak("According to Wikipedia")
+            print(results)
+            speak(results)
+
+        elif 'open youtube' in query:
+            webbrowser.open("youtube.com")
+
+        elif 'open google' in query:
+            webbrowser.open("google.com")
+
+        elif 'open wikipedia' in query:
+            webbrowser.open("wikipedia.com")
+
+        elif 'open free code camp' in query:
+            webbrowser.open("freecodecamp.com")
+
+        elif 'the time' in query:
+            strTime = datetime.datetime.now().strftime("%H:%M:%S")
+            speak(f"Sir, the time is {strTime}")
